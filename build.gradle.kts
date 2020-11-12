5203e9286fb64303c9ad8a76aa440b7d55b302df
Tyler Wong <tbwong3@gmail.com>
2020-11-12 12:20:43 -0800

    Update embedded AGP to stable

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:4.2.0-alpha16")
+        classpath("com.android.tools.build:gradle:4.1.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
         classpath("me.tylerbwong.gradle:metalava-gradle:0.1.0-alpha06")
     }
--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -53,6 +53,6 @@ metalava {
 dependencies {
     implementation(gradleApi())
     implementation(kotlin("stdlib"))
-    implementation("com.android.tools.build:gradle:4.2.0-alpha16")
+    implementation("com.android.tools.build:gradle:4.1.1")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
 }
