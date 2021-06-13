d34420f1aa3aa759d1d8340b49a26a7fdccc25fe
Tyler Wong <tbwong3@gmail.com>
2021-06-12 20:24:26 -0700

    Add Release Workflow

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -57,3 +57,29 @@ dependencies {
     compileOnly(kotlin("stdlib"))
     compileOnly(libs.android.gradlePlugin)
 }
+
+/**
+ * Attempts to grab Gradle Publish credentials from environment variables if the properties are not
+ * present. Will be done before plugin publish.
+ */
+val setupPublishCredentialsTask = tasks.register("setupPublishCredentials") {
+    doFirst {
+        val keyProperty = "gradle.publish.key"
+        val secretProperty = "gradle.publish.secret"
+
+        if (!project.hasProperty(keyProperty) || !project.hasProperty(secretProperty)) {
+            logger.warn("`$keyProperty` or `$secretProperty` were not set. Attempting to configure from environment variables")
+
+            val key = System.getenv("GRADLE_PUBLISH_KEY")
+            val secret = System.getenv("GRADLE_PUBLISH_SECRET")
+            if (key != null && secret != null) {
+                System.setProperty(keyProperty, key)
+                System.setProperty(secretProperty, secret)
+            } else {
+                logger.warn("Publishing key or secret was null")
+            }
+        }
+    }
+}
+
+tasks.getByName("publishPlugins").dependsOn(setupPublishCredentialsTask)
