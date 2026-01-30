61361329bceb082964a71abb82bd04be2f87b7f7
Goooler <wangzongler@gmail.com>
2026-01-30 18:10:15 +0800

    Migrate samples to AGP 9

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,36 @@
+import com.android.build.api.dsl.CommonExtension
+import com.android.build.gradle.api.AndroidBasePlugin
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.android.kmpLibrary) apply false
     alias(libs.plugins.kotlin.compiler) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
+}
+
+allprojects {
+    plugins.withType<AndroidBasePlugin>().configureEach {
+        extensions.configure<CommonExtension> {
+            compileOptions.apply {
+                sourceCompatibility(libs.versions.jvmTarget.get())
+                targetCompatibility(libs.versions.jvmTarget.get())
+            }
+        }
+    }
+
+    plugins.withType<JavaBasePlugin>().configureEach {
+        extensions.configure<JavaPluginExtension> {
+            setSourceCompatibility(libs.versions.jvmTarget.get())
+            setTargetCompatibility(libs.versions.jvmTarget.get())
+        }
+    }
+
+    tasks.withType<KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget = JvmTarget.fromTarget(libs.versions.jvmTarget.get())
+        }
+    }
 }
--- samples/kotlin-multiplatform/build.gradle.kts
+++ samples/kotlin-multiplatform/build.gradle.kts
@@ -1,14 +1,15 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.android.library)
+    alias(libs.plugins.android.kmpLibrary)
     alias(libs.plugins.metalava)
 }
 
 kotlin {
-    androidTarget {
-        compilerOptions.jvmTarget = JvmTarget.JVM_17
+    android {
+        namespace = "me.tylerbwong.gradle.metalava.sample"
+        compileSdk = 34
+        minSdk = 21
+        androidResources.enable = true
     }
     iosX64()
     iosArm64()
@@ -18,24 +19,6 @@ kotlin {
     }
     jvm()
     linuxX64()
-}
-
-android {
-    namespace = "me.tylerbwong.gradle.metalava.sample"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 21
-    }
-
-    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-    sourceSets["main"].res.srcDirs("src/androidMain/res")
-    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
 }
 
 metalava {
