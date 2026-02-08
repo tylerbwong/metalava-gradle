ffc696663902bf43face387453ec99cbe3d52dab
Goooler <wangzongler@gmail.com>
2026-02-08 15:26:11 +0800

    Tweak CI configs a bit

--- samples/groovy-java/src/main/java/me/tylerbwong/gradle/metalava/sample/SampleJavaPublicApi.java
+++ samples/groovy-java/src/main/java/me/tylerbwong/gradle/metalava/sample/SampleJavaPublicApi.java
@@ -1,7 +1,6 @@
 package me.tylerbwong.gradle.metalava.sample;
 
 public abstract class SampleJavaPublicApi {
-    public abstract String publicStringProperty;
     public abstract void publicFunction();
 
     public static int publicStaticIntFunction(int value) {
