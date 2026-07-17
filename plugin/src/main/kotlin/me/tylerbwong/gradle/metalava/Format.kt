7375618ad9e3c89e699828c65ba4451bcea0e022
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2026-07-17 14:15:23 +0100

    Use numeric Metalava format versions
    Metalava 1.0.0-alpha15 removed legacy format labels such as v4. Numeric specifiers work across the plugin's alpha10 through alpha15 compatibility range.
    
    Fixes https://github.com/tylerbwong/metalava-gradle/issues/181

--- plugin/src/main/kotlin/me/tylerbwong/gradle/metalava/Format.kt
+++ plugin/src/main/kotlin/me/tylerbwong/gradle/metalava/Format.kt
@@ -1,10 +1,12 @@
 package me.tylerbwong.gradle.metalava
 
 /** Sets the output signature file format to be the given version. */
-public enum class Format {
-    V2,
-    V3,
-    V4;
+public enum class Format(
+    private val version: String
+) {
+    V2("2.0"),
+    V3("3.0"),
+    V4("4.0");
 
-    override fun toString(): String = name.lowercase()
+    override fun toString(): String = version
 }
